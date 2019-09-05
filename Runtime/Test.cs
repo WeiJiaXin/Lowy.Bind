707964349eb8925cf3846788a5bfb0c8e7d4cf26
Lowy <1428416750@qq.com>
2019-09-05 12:14:26 +0800

    增加项目文件

---
+++ Runtime/Test.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lowy.Bind;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Awake()
+    {
+        Binder.Bind<IInterface>().To<MyClass2>().ToName(MyEnum._1);
+        Binder.Bind<IInterface>().To<MyClass>();
+        Binder.GetBind<IInterface>().ToSingleForAll();
+        Binder.ReflectionAll();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(Binder.GetInstance<IInterface>(MyEnum._1,"strTest").GetType());
+        print(Binder.GetInstance<IInterface>()+"--");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public enum MyEnum
+{
+    _1,_2
+}
+public interface IInterface
+{
+    string s { get; set; }
+}
+public class MyClass:IInterface
+{
+    public string s { get; set; }
+
+    public MyClass()
+    {
+        Debug.Log("MyuClass");
+    }
+}
+public class MyClass2 : IInterface
+{
+    public string s { get; set; }
+    [Inject]
+    public IInterface inf;
+    private MyClass2(string s)
+    {
+        this.s = s;
+        Debug.Log(s);
+        Binder.InjectObj(this);
+    }
+}
